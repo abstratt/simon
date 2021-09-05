d730ff49bed08bca83003be13bbb37ac4d045da1
Rafael Chaves <rafael@abstratt.com>
2021-09-05 20:55:45 -0300

    releng

--- update-javadoc.sh
+++ update-javadoc.sh
@@ -1,5 +1,6 @@
 # Credit: https://vaadin.com/blog/host-your-javadoc-s-online-in-github
 set -x 
+mvn clean install
 mvn clean javadoc:javadoc javadoc:aggregate
 cd target/site/apidocs/javadoc/
 git init
@@ -9,4 +10,6 @@ git add --all
 git commit -m sync
 git merge --allow-unrelated-histories --no-edit -s ours remotes/javadoc/gh-pages
 git push --set-upstream javadoc master:gh-pages --force
-rm -Rf .git
+pwd
+cd -
+mvn clean -N
