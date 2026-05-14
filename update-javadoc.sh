e6a9d20d06ceb378170d4fc388cd884d7dedd6bd
Rafael Chaves <rchaves@gradle.com>
2026-05-14 07:22:06 -0300

    Remove references to manifold and try to fix javadoc generation

--- update-javadoc.sh
+++ update-javadoc.sh
@@ -1,10 +1,10 @@
 # Credit: https://vaadin.com/blog/host-your-javadoc-s-online-in-github
 set -x 
 mvn clean
-mvn clean javadoc:javadoc javadoc:aggregate
+mvn javadoc:javadoc javadoc:aggregate
 mvn com.github.ferstl:depgraph-maven-plugin:aggregate  "-Dincludes=com.abstratt.simon:*"
-dot -Tpng target/dependency-graph.dot -o target/site/apidocs/javadoc/dependencies.png
-cd target/site/apidocs/javadoc/
+dot -Tpng target/dependency-graph.dot -o target/reports/apidocs/dependencies.png
+cd target/reports/apidocs/
 git init
 git remote add javadoc git@github.com:abstratt/simon.git
 git fetch --depth=1 javadoc gh-pages 
